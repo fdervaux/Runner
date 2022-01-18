d33ab18a998486282e60c12d345b07548e122e85
francois dervaux <francois.dervaux@gmail.com>
2022-01-18 16:44:38 +0100

    add comments

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,7 +7,8 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // j'ajoute un super commentaire
+        // sur plusieurs ligne
     }
 
     // Update is called once per frame
